19555784e92dc7de2f5e545e3726f19f9d10ab78
Krzysztof Stężała <krzste09@outlook.com>
2018-05-19 14:42:58 +0200

    aha

--- js-blockchain-test/main.js
+++ js-blockchain-test/main.js
@@ -16,3 +16,4 @@ class Block {
 
     }
 }
+
---
+++ twitter-client/js/main.js
@@ -0,0 +1,16 @@
+//configure Twitter API
+//create search form
+//analyze search form 
+//find regular expressions -> advanced regex
+/*
+advanced regex looking for 
+- words
+- numbers
+- expressions (collocations of words)
+- dates (different formats supported)
+- 
+*/
+//form for entering tags
+//tags detection mechanism -> keywords-to-tags
+//
+ 
