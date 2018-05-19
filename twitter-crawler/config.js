cb6281a7123e00e9b1474afee53a8bfb6fd4c3d4
Krzysztof Stężała <krzste09@outlook.com>
2018-05-19 19:04:51 +0200

    blabla

---
+++ twitter-crawler/config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: '4bD8nnDCCnKI95z48WOXhQZbJ',  
+    consumer_secret: 'tvjp7pz9I1CNAz6ApLLz3k3YURif1bIKxj6liVMqu6RTbXiHtZ',
+    access_token: '971669050131664896-t2dDcyeVcMGaoZWWs8m1Y0FE6LjyiqK',  
+    access_token_secret: 'yYNX8f4Wk9VJ0iaFy1cBsP6Q3jl0ggzL8Km4DpyssKAYi'
+  }
+
